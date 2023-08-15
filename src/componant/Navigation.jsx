f3d71086826a64b0aaac025c5596356801ce26ea
vivekkamble1419 <mrvivekkamble8@gmail.com>
2023-08-15 16:02:40 +0530

    All necessary things implimented

---
+++ src/componant/Navigation.jsx
@@ -0,0 +1,20 @@
+function Navigation() {
+  return (
+    <>
+        <nav className="container">
+        <div className='logo'>
+          <img src='/Images/nikelogo.png' alt='logo'></img>
+        </div>
+        <ul>
+          <li href="#">HOME</li>
+          <li href="#">LOCATION</li>
+          <li href="#">ABOUT</li>
+          <li href="#">CONTACT</li>
+        </ul>
+        <button>Login</button>
+      </nav>     
+    </>
+  )
+}
+
+export default Navigation
---
+++ src/componant/Nike.jsx
@@ -0,0 +1,29 @@
+
+export const Nike = () => {
+  return (
+    
+        <main className="nike container">
+            <div className="nike-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati alias odio inventore voluptate quae provident aliquid culpa sint dicta animi aut fugit aspernatur in quibusdam soluta, eum maiores totam adipisci?
+                </p>
+                <div className="nike-button">
+                    <button>Shop Now</button>
+                    <button className="secondary-btn">Categary</button>
+                </div>
+                <div className="shopping">
+                    <p>Avalable On</p>
+                    <div className="brand-icons">
+                        <img src="/Images/flipcart.avif" alt="flipcart" />
+                        <img src="/Images/amazon.png" alt="amazon" />
+                    </div>
+                </div>
+            </div>
+            <div className="shoo-image">
+
+            <img src="/Images/shoe-running.png" alt="Shouse" />
+            </div>
+        </main>
+    
+  )
+}
